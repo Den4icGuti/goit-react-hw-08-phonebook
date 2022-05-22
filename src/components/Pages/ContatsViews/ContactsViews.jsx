73599f4822ee79e6b3f4434427e115ae96123c21
Den4icGuti <goos7412@gmail.com>
2022-05-22 13:38:03 +0300

    add page contacts

--- src/components/Pages/ContatsViews/ContactsViews.jsx
+++ src/components/Pages/ContatsViews/ContactsViews.jsx
@@ -1,10 +1,10 @@
 import Form from '../Contacts/Filter/Form/Form';
 import Filter from '../Contacts/Filter/Filter';
 import UserList from '../Contacts/Filter/UserList/UserList';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 const ContactsViews = () => {
-  const [data, setData] = useState([]);
+  const data = [];
   return (
     <div>
       <Form />
